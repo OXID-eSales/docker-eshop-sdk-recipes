3bb2ca461ce2f319937ef9e6496cc95d5b49ab97
Anton Fedurtsya <anton@fedurtsya.com>
2022-01-26 23:34:58 +0200

    Add b-6.4.x shop recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # development-oxid-recipes
 Oxid eShop setup recipes
+
+## Installation instructions:
+
+```
+    mkdir myNewShinyProject && cd myNewShinyProject
+    git clone https://github.com/Fresh-Advance/development.git .
+    git clone https://github.com/Fresh-Advance/development-oxid-recipes.git recipes/oxid-esales
+    
+    ./recipes/oxid-esales/b-6.4.x-dev/run.sh
+```
---
+++ b-6.4.x-dev/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
+
+cd $SCRIPT_PATH/../../../ || exit
+
+git clone https://github.com/OXID-eSales/oxideshop_ce.git --branch=b-6.4.x source
+
+make setup
+sed "s/display_errors =.*/display_errors = false/" -i containers/php-fpm/custom.ini
+sed -i -e "s+/var/www/+/var/www/source/+" containers/httpd/project.conf
+
+cp source/source/config.inc.php.dist source/source/config.inc.php
+sed -i "1s+^+SetEnvIf Authorization "\(.*\)" HTTP_AUTHORIZATION=\$1\n\n+" source/source/.htaccess
+sed -i -e 's/<dbHost>/mysql/'\
+       -e 's/<dbUser>/root/'\
+       -e 's/<dbName>/example/'\
+       -e 's/<dbPwd>/root/'\
+       -e 's/<dbPort>/3306/'\
+       -e 's/<sShopURL>/http:\/\/localhost.local\//'\
+       -e 's/<sShopDir>/\/var\/www\/source\//'\
+       -e 's/<sCompileDir>/\/var\/www\/source\/tmp\//'\
+    source/source/config.inc.php
+
+make up
+
+docker-compose exec -T php composer update --no-interaction
+docker-compose exec -T php php vendor/bin/reset-shop
+
+echo "Done!"
