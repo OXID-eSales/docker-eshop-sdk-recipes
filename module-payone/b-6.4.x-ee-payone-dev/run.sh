22305df6e67792151a0a83e5657c52b0cb6c8a2d
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-03-09 11:37:38 +0200

    Prepare payone recipe

---
+++ module-payone/b-6.4.x-ee-payone-dev/run.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
+
+cd $SCRIPT_PATH/../../../../ || exit
+
+git clone https://github.com/OXID-eSales/oxideshop_ce.git --branch=b-6.4.x source
+
+# Prepare services configuration
+make setup
+make addbasicservices
+
+# Configure containers
+perl -pi\
+  -e 's#/var/www/#/var/www/source/#g;'\
+  containers/httpd/project.conf
+
+# Configure shop
+cp source/source/config.inc.php.dist source/source/config.inc.php
+
+perl -pi\
+  -e 'print "SetEnvIf Authorization \"(.*)\" HTTP_AUTHORIZATION=\$1\n\n" if $. == 1'\
+  source/source/.htaccess
+
+perl -pi\
+  -e 's#<dbHost>#mysql#g;'\
+  -e 's#<dbUser>#root#g;'\
+  -e 's#<dbName>#example#g;'\
+  -e 's#<dbPwd>#root#g;'\
+  -e 's#<dbPort>#3306#g;'\
+  -e 's#<sShopURL>#http://localhost.local/#g;'\
+  -e 's#<sShopDir>#/var/www/source/#g;'\
+  -e 's#<sCompileDir>#/var/www/source/tmp/#g;'\
+  source/source/config.inc.php
+
+# Clone Payone module to modules directory
+git clone git@github.com:PAYONE-GmbH/oxid-6.git --branch=v1.6.2 source/source/modules/fc/fcpayone
+
+# Start all containers
+make up
+
+docker-compose exec php composer config repositories.oxid-esales/oxideshop-ee git https://github.com/OXID-eSales/oxideshop_ee
+docker-compose exec php composer config repositories.oxid-esales/oxideshop-pe git https://github.com/OXID-eSales/oxideshop_pe
+docker-compose exec php composer require oxid-esales/oxideshop-pe:dev-b-6.4.x --no-update
+docker-compose exec php composer require oxid-esales/oxideshop-ee:dev-b-6.4.x --no-update
+
+# Configure modules in composer
+docker-compose exec -T \
+  php composer config repositories.payone-gmbh/oxid-6 \
+  --json '{"type":"path", "url":"./source/modules/fc/fcpayone", "options": {"symlink": true}}'
+docker-compose exec -T php composer require payone-gmbh/oxid-6:* --no-update
+
+docker-compose exec -T php composer update --no-interaction
+docker-compose exec -T php php vendor/bin/reset-shop
+
+docker-compose exec -T php bin/oe-console oe:module:install-configuration source/modules/fc/fcpayone/
+docker-compose exec -T php bin/oe-console oe:module:activate fcpayone
+
+echo "Done!"
