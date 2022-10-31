dd964077eb25723633d1db6592eeaf42d70349e5
j.skoczek <j.skoczek@createit.pl>
2022-10-31 09:27:38 +0100

    fixup

--- module-usercentrics/b-7.0.x-ee-usercentrics-dev/run.sh
+++ module-usercentrics/b-7.0.x-ee-usercentrics-dev/run.sh
@@ -37,7 +37,7 @@ perl -pi\
   -e 's#<sShopURL>#http://localhost.local/#g;'\
   -e 's#<sShopDir>#/var/www/source/#g;'\
   -e 's#<sCompileDir>#/var/www/source/tmp/#g;'\
-  source/source/config.inc.phpp
+  source/source/config.inc.php
 
 # Clone Usercentrics module to modules directory
 git clone https://github.com/OXID-eSales/usercentrics.git --branch=b-7.0.x source/source/modules/oxps/usercentrics
@@ -60,9 +60,9 @@ docker-compose exec php composer config repositories.oxid-esales/twig-component-
 
 docker-compose exec php composer require oxid-esales/twig-theme:dev-b-7.0.x --no-update
 docker-compose exec php composer require oxid-esales/twig-admin-theme:dev-b-7.0.x --no-update
-docker-compose exec php composer require oxid-esales/twig-component:dev-b-7.0.x --no-update
-docker-compose exec php composer require oxid-esales/twig-component-pe:dev-b-7.0.x --no-update
-docker-compose exec php composer require oxid-esales/twig-component-ee:dev-b-7.0.x --no-update
+docker-compose exec php composer require oxid-esales/twig-component:^v2.0 --no-update
+docker-compose exec php composer require oxid-esales/twig-component-^v2.0 --no-update
+docker-compose exec php composer require oxid-esales/twig-component-^v2.0 --no-update
 
 docker-compose exec php composer require oxid-esales/oxideshop-pe:dev-b-7.0.x --no-update
 docker-compose exec php composer require oxid-esales/oxideshop-ee:dev-b-7.0.x --no-plugins --no-scripts
