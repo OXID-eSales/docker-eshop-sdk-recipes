b5c2bb179bab798b6e955c62cd0c58dfdd79fd4a
Tomasz Kowalewski <t.kowalewski@createit.pl>
2023-01-31 12:16:57 +0100

    Add recipe for module-template b-7.0 EE Twig

---
+++ module-template/b-7.0.x-ee-twig-dev/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
+
+cd $SCRIPT_PATH/../../../../ || exit
+
+# Prepare services configuration
+make setup
+make addbasicservices
+make file=services/selenium-chrome.yml addservice
+
+$SCRIPT_PATH/../../parts/b-7.0.x/start_shop.sh -eEE
+$SCRIPT_PATH/../../parts/b-7.0.x/require_twig_components.sh -eEE
+
+# Configure modules in composer
+git clone https://github.com/OXID-eSales/module-template.git --branch=b-7.0.x source/source/modules/oe/moduletemplate
+docker-compose exec -T \
+  php composer config repositories.oxid-esales/module-template \
+  --json '{"type":"path", "url":"./source/modules/oe/moduletemplate", "options": {"symlink": true}}'
+docker-compose exec -T php composer require oxid-esales/module-template:* --no-update
+
+docker-compose exec -T \
+  php composer config repositories.oxid-esales/oxideshop-demodata-ee \
+  --json '{"type":"git", "url":"https://github.com/OXID-eSales/oxideshop_demodata_ee"}'
+docker-compose exec -T php composer require oxid-esales/oxideshop-demodata-ee:dev-master --no-update
+
+# Install all preconfigured dependencies
+docker-compose exec -T php composer update --no-interaction
+
+$SCRIPT_PATH/../../parts/b-7.0.x/reset_database.sh -eEE
+docker-compose exec -T php bin/oe-console oe:setup:demodata
+docker-compose exec -T php bin/oe-console oe:admin:create --admin-email='admin@admin.com' --admin-password='admin'
+
+docker-compose exec -T php bin/oe-console oe:module:activate oe_moduletemplate
+
+echo "Done! Admin login: admin@admin.com Password: admin"
