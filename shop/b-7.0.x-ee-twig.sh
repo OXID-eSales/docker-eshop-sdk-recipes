99ec1336d7c1f7feb5ea7b6d4d897a20b4ebae60
Anton Fedurtsya <anton@fedurtsya.com>
2023-03-09 13:55:46 +0200

    Update shop twig theme recipe to use parts
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ shop/b-7.0.x-ee-twig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
+
+cd $SCRIPT_PATH/../../../ || exit
+
+# Prepare services configuration
+make setup
+make addbasicservices
+make file=services/selenium-chrome.yml addservice
+make file=services/node.yml addservice
+
+$SCRIPT_PATH/../parts/b-7.0.x/start_shop.sh -eEE
+$SCRIPT_PATH/../parts/b-7.0.x/require_twig_components.sh -eEE -ttwig
+
+# Require demodata package
+docker-compose exec -T \
+  php composer config repositories.oxid-esales/oxideshop-demodata-ee \
+  --json '{"type":"git", "url":"https://github.com/OXID-eSales/oxideshop_demodata_ee"}'
+docker-compose exec -T php composer require oxid-esales/oxideshop-demodata-ee:dev-master --no-update
+
+# Install all preconfigured dependencies
+docker-compose exec -T php composer update --no-interaction
+
+$SCRIPT_PATH/../parts/b-7.0.x/reset_database.sh -eEE
+docker-compose exec -T php bin/oe-console oe:setup:demodata
+docker-compose exec -T php bin/oe-console oe:admin:create --admin-email='admin@admin.com' --admin-password='admin'
+
+docker-compose exec -T php bin/oe-console oe:module:activate ddoewysiwyg
+docker-compose exec -T php bin/oe-console oe:theme:activate twig
+
+echo "Done! Admin login: admin@admin.com Password: admin"
