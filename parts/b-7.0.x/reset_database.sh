23c34a448a2beafcbd051f8c3ea66f37ef085a17
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-07-24 12:18:31 +0200

    OXDEV-7162: Remove shop-id flag from reset_database.sh

--- parts/b-7.0.x/reset_database.sh
+++ parts/b-7.0.x/reset_database.sh
@@ -3,7 +3,6 @@
 # Flags possible: -e for edition. Example: -eEE
 
 edition="CE"
-shop_flag=""
 
 while getopts e: flag; do
   case "${flag}" in
@@ -11,8 +10,4 @@ while getopts e: flag; do
   esac
 done
 
-if [ $edition = "EE" ]; then
-  shop_flag="--shop-id=1"
-fi
-
-docker-compose exec -T php bin/oe-console oe:database:reset --db-host=mysql --db-port=3306 --db-name=example --db-user=root --db-password=root ${shop_flag} --force
+docker-compose exec -T php bin/oe-console oe:database:reset --db-host=mysql --db-port=3306 --db-name=example --db-user=root --db-password=root --force
