60f82467b00c9f2c8285065257e786531e2eb1f5
Anton Fedurtsya <anton@fedurtsya.com>
2023-07-31 13:37:06 +0300

    Add multi-edition e-vat module twig recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- module-e-vat/b-7.0.x-twig-dev.sh
+++ module-e-vat/b-7.0.x-twig-dev.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Flags possible:
+# -e for shop edition. Possible values: CE/PE/EE
+
+edition='EE'
+while getopts e: flag; do
+  case "${flag}" in
+  e) edition=${OPTARG} ;;
+  *) ;;
+  esac
+done
+
 SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
 
-cd $SCRIPT_PATH/../../../../ || exit
+cd $SCRIPT_PATH/../../../ || exit
 
 # Prepare services configuration
 make setup
@@ -11,27 +22,20 @@ make file=services/adminer.yml addservice
 make file=services/selenium-chrome.yml addservice
 make file=services/node.yml addservice
 
-$SCRIPT_PATH/../../parts/b-7.0.x/start_shop.sh -eEE
-$SCRIPT_PATH/../../parts/shared/require_twig_components.sh -e"EE" -b"b-7.0.x"
-$SCRIPT_PATH/../../parts/shared/require_theme.sh -t"twig" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/b-7.0.x/start_shop.sh -e"{$edition}"
+$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"{$edition}" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require_theme_dev.sh -t"apex" -b"b-7.0.x"
 
-# Require demodata package
-docker compose exec -T \
-  php composer config repositories.oxid-esales/oxideshop-demodata-ee \
-  --json '{"type":"git", "url":"https://github.com/OXID-eSales/oxideshop_demodata_ee"}'
-docker compose exec -T php composer require oxid-esales/oxideshop-demodata-ee:dev-b-7.0.x --no-update
+$SCRIPT_PATH/../parts/shared/require_demodata_package.sh -e"{$edition}" -b"b-7.0.x"
 
-# Clone eVat module to modules directory
-git clone https://github.com/OXID-eSales/vat_tbe_services.git --branch=b-7.0.x source/dev-packages/oevattbe
-
-# Clone eVat documentation to source directory
+# Configure eVat documentation
 git clone https://github.com/OXID-eSales/vat-tbe-services-documentation.git --branch=2.1-en source/docs
-
-# Add Sphinx container
 make docpath=./source/docs addsphinxservice
+
 make up
 
-# Configure module in composer
+# Clone and configure module package in composer
+git clone https://github.com/OXID-eSales/vat_tbe_services.git --branch=b-7.0.x source/dev-packages/oevattbe
 docker compose exec -T \
   php composer config repositories.oxid-esales/evat-module \
   --json '{"type":"path", "url":"./dev-packages/oevattbe", "options": {"symlink": true}}'
@@ -40,11 +44,11 @@ docker compose exec -T php composer require oxid-esales/evat-module:* --no-updat
 # Install all preconfigured dependencies
 docker compose exec -T php composer update --no-interaction
 
-$SCRIPT_PATH/../../parts/shared/setup_database.sh
+$SCRIPT_PATH/../parts/shared/setup_database.sh
 
 docker compose exec -T php bin/oe-console oe:module:activate oevattbe
 docker compose exec -T php bin/oe-console oe:theme:activate twig
 
-$SCRIPT_PATH/../../parts/shared/create_admin.sh
+$SCRIPT_PATH/../parts/shared/create_admin.sh
 
 echo "Done!"
