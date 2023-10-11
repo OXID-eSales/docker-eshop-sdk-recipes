23a209fd68a9de718ea4efc660ff37950ae9dcdb
Anton Fedurtsya <anton@fedurtsya.com>
2023-10-11 17:17:58 +0300

    Cleanup b-7.0.x recipes to have one based on -e flag
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- shop/b-7.0.x-apex-dev.sh
+++ shop/b-7.0.x-apex-dev.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Flags possible:
+# -e for shop edition. Possible values: CE/PE/EE
+
+edition='EE'
+while getopts e: flag; do
+  case "${flag}" in
+  e) edition=${OPTARG} ;;
+  *) ;;
+  esac
+done
+
 SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
 cd $SCRIPT_PATH/../../../ || exit
 
@@ -10,12 +21,12 @@ make file=services/adminer.yml addservice
 make file=services/selenium-chrome.yml addservice
 make file=services/node.yml addservice
 
-$SCRIPT_PATH/../parts/b-7.0.x/start_shop.sh -e"EE"
-$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"EE" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/b-7.0.x/start_shop.sh -e"${edition}"
+$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"${edition}" -b"b-7.0.x"
 
-$SCRIPT_PATH/../parts/shared/require_theme.sh -t"apex" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require_theme_dev.sh -t"apex" -b"b-7.0.x"
 
-${SCRIPT_PATH}/../parts/shared/require_demodata_package.sh -e"EE" -b"b-7.0.x"
+${SCRIPT_PATH}/../parts/shared/require_demodata_package.sh -e"${edition}" -b"b-7.0.x"
 
 # Install all preconfigured dependencies
 docker compose exec -T php composer update --no-interaction
