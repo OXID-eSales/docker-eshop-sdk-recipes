91257394c6254488be0183edf6eceaecc50352e8
Anton Fedurtsya <anton@fedurtsya.com>
2023-10-19 16:00:58 +0300

    Change the old apex module template to new syntax

--- module-template/b-7.0.x-apex-dev.sh
+++ module-template/b-7.0.x-apex-dev.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
+# Flags possible:
+# -e for shop edition. Possible values: CE/PE/EE
+
+edition='EE'
+while getopts e: flag; do
+  case "${flag}" in
+  e) edition=${OPTARG} ;;
+  *) ;;
+  esac
+done
+
 SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
 
-cd $SCRIPT_PATH/../../../../ || exit
+cd $SCRIPT_PATH/../../../ || exit
 
 # Prepare services configuration
 make setup
 make addbasicservices
-make file=services/node.yml addservice
+make file=services/adminer.yml addservice
 make file=services/selenium-chrome.yml addservice
+make file=services/node.yml addservice
 
-perl -pi\
-  -e 's#node:latest#node:12#g;'\
-  docker-compose.yml
+$SCRIPT_PATH/../parts/b-7.0.x/start_shop.sh -e"${edition}"
+$SCRIPT_PATH/../parts/shared/require_twig_components.sh -e"${edition}" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require_theme_dev.sh -t"apex" -b"b-7.0.x"
 
-$SCRIPT_PATH/../../parts/b-7.0.x/start_shop.sh -eCE
-$SCRIPT_PATH/../../parts/shared/require_twig_components.sh -e"CE" -b"b-7.0.x"
-$SCRIPT_PATH/../../parts/shared/require_theme.sh -t"apex" -b"b-7.0.x"
+$SCRIPT_PATH/../parts/shared/require_demodata_package.sh -e"${edition}" -b"b-7.0.x"
+
+make up
 
 # Configure modules in composer
 git clone https://github.com/OXID-eSales/module-template.git --branch=b-7.0.x source/dev-packages/moduletemplate
@@ -25,19 +37,14 @@ docker compose exec -T \
   --json '{"type":"path", "url":"./dev-packages/moduletemplate", "options": {"symlink": true}}'
 docker compose exec -T php composer require oxid-esales/module-template:* --no-update
 
-docker compose exec -T \
-  php composer config repositories.oxid-esales/oxideshop-demodata-ce \
-  --json '{"type":"git", "url":"https://github.com/OXID-eSales/oxideshop_demodata_ce"}'
-docker compose exec -T php composer require oxid-esales/oxideshop-demodata-ce:dev-b-7.0.x --no-update
-
 # Install all preconfigured dependencies
 docker compose exec -T php composer update --no-interaction
 
-$SCRIPT_PATH/../../parts/shared/setup_database.sh
+$SCRIPT_PATH/../parts/shared/setup_database.sh
 
 docker compose exec -T php bin/oe-console oe:module:activate oe_moduletemplate
 docker compose exec -T php bin/oe-console oe:theme:activate apex
 
-$SCRIPT_PATH/../../parts/shared/create_admin.sh
+$SCRIPT_PATH/../parts/shared/create_admin.sh
 
 echo "Done!"
