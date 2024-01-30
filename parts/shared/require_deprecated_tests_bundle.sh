83a7380b0f4a63705c9786090bd40f1e741802f8
Anton Fedurtsya <anton@fedurtsya.com>
2024-01-30 16:33:14 +0200

    Add a shop 7.1 recipe with deprecated tests bundle

--- parts/shared/require_deprecated_tests_bundle.sh
+++ parts/shared/require_deprecated_tests_bundle.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Warning! This part is reconfiguring the composer requirements to the OLD versions so it will fit Testing library!
+# Usage of this part is very limited, and cannot be combined with development versions of the shop.
+# Decide what you want to work with - this deprecated testsuite, or the new one - dont use this part then.
+
 # Flags possible:
 # -e for shop edition. Possible values: CE/PE/EE
 # -b branch of deprecated tests repositories
@@ -18,6 +22,13 @@ if [ -z ${edition+x} ] || [ -z ${branch+x} ]; then
   echo -e "\e[1;31mThe edition (-e) and branch (-b) are required for require_deprecated_tests_bundle.sh\e[0m"
   exit 1
 fi
+
+docker compose exec -T php composer require phpunit/phpunit:^9.1.1 --no-update
+docker compose exec -T php composer require codeception/module-rest:^3.0.0 --no-update
+docker compose exec -T php composer require codeception/module-phpbrowser:^3.0.0 --no-update
+docker compose exec -T php composer require codeception/module-asserts:^3.0 --no-update
+docker compose exec -T php composer require codeception/module-webdriver:^3.1 --no-update
+docker compose exec -T php composer require codeception/module-db:^3.0 --no-update
 
 docker compose exec php composer require oxid-esales/tests-deprecated-ce:dev-${branch} --with-all-dependencies --no-update
 
