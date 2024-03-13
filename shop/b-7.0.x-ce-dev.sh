c719819a510fd17263edd22491512217ad539a6d
marcelmanzel <marcel.manzel@oxid-esales.com>
2024-03-13 13:57:45 +0100

    OXDEV-7845: Fix recipe because of forgotten debugging changes

--- shop/b-7.0.x-ce-dev.sh
+++ shop/b-7.0.x-ce-dev.sh
@@ -19,22 +19,7 @@ perl -pi\
   -e 's#/var/www/#/var/www/source/#g;'\
   containers/httpd/project.conf
 
-mkdir source
-cp $SCRIPT_PATH/../parts/shared/composer.json.base ../../../source/composer.json
-
 docker compose up --build -d php
-
-docker compose exec php composer init --no-interaction --name="oxid-esales/oxideshop-project" --stability=dev
-
-docker compose exec php composer config github-protocols https
-docker compose exec php composer config allow-plugins.oxid-esales/oxideshop-composer-plugin true
-docker compose exec php composer config allow-plugins.oxid-esales/oxideshop-unified-namespace-generator true
-docker compose exec php composer config preferred-install.oxid-esales/* source
-
-
-# Define sample multi-line literal.
-replace='Laurel & Hardy; PS\2
-Masters\1 & Johnson\2'
 
 AUTOLOAD_DEV='"autoload-dev": {
       "psr-4": {
@@ -47,7 +32,7 @@ AUTOLOAD_DEV='"autoload-dev": {
 IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$AUTOLOAD_DEV")
 AUTOLOAD_DEV_ESCAPED=${REPLY%$'\n'}
 
-sed -e "s/\<autoloadDev>/${AUTOLOAD_DEV_ESCAPED}/" ${SCRIPT_PATH}/parts/shared/composer.json.base > $SCRIPT_PATH/../../source/composer.json
+sed -e "s/\<autoloadDev>/${AUTOLOAD_DEV_ESCAPED}/" ${SCRIPT_PATH}/../parts/shared/composer.json.base > ./source/composer.json
 
 docker compose exec php composer config repositories.oxid-esales/oxideshop-ce git https://github.com/OXID-eSales/oxideshop_ce.git
 docker compose exec php composer require oxid-esales/oxideshop-ce:dev-b-7.0.x --no-update
