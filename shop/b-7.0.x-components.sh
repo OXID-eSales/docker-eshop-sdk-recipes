f21a4896f8dddb54aa3cb01174b02264219449b0
Anton Fedurtsya <anton@fedurtsya.com>
2024-03-22 17:43:05 +0200

    OXDEV-7845 Add graphql configuration module and improve shop components recipes
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- shop/b-7.0.x-components.sh
+++ shop/b-7.0.x-components.sh
@@ -19,6 +19,7 @@ perl -pi\
   -e 's#/var/www/#/var/www/source/#g;'\
   containers/httpd/project.conf
 
+mkdir source
 docker compose up --build -d php
 
 AUTOLOAD_DEV='
@@ -26,24 +27,21 @@ AUTOLOAD_DEV='
         "OxidEsales\\\\EshopCommunity\\\\Tests\\\\": "./vendor/oxid-esales/oxideshop-ce/tests"
       }'
 
-cp ${SCRIPT_PATH}/../parts/shared/composer.json.base ./source/composer.json
+cp ${SCRIPT_PATH}/../parts/bases/composer.json.base ./source/composer.json
 perl -pi\
   -e "s#\"autoload-dev\": {#\"autoload-dev\":{${AUTOLOAD_DEV}#g;"\
   ./source/composer.json
 
-## Escape it for use as a Sed replacement string.
-## (https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed)
-#IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$AUTOLOAD_DEV")
-#AUTOLOAD_DEV_ESCAPED=${REPLY%$'\n'}
-#
-#sed -e "s/\<autoloadDev>/${AUTOLOAD_DEV_ESCAPED}/" ${SCRIPT_PATH}/../parts/shared/composer.json.base > ./source/composer.json
-
 docker compose exec php composer config repositories.oxid-esales/oxideshop-ce git https://github.com/OXID-eSales/oxideshop_ce.git
+docker compose exec php composer config repositories.oxid-esales/oxideshop-pe git https://github.com/OXID-eSales/oxideshop_pe.git
+docker compose exec php composer config repositories.oxid-esales/oxideshop-ee git https://github.com/OXID-eSales/oxideshop_ee.git
+
 docker compose exec php composer require oxid-esales/oxideshop-ce:dev-b-7.0.x --no-update
+docker compose exec php composer require oxid-esales/oxideshop-pe:dev-b-7.0.x --no-update
+docker compose exec php composer require oxid-esales/oxideshop-ee:dev-b-7.0.x --no-update
 docker compose exec php composer require oxid-esales/developer-tools:dev-b-7.0.x --no-update
 
-docker compose exec php composer require oxid-esales/graphql-configuration-access:dev-b-7.0.x-new_dev_recipes-OXDEV-7845
-
+$SCRIPT_PATH/../parts/shared/require_theme.sh -t"twig-admin" -b"b-7.0.x"
 $SCRIPT_PATH/../parts/shared/require_theme.sh -t"apex" -b"b-7.0.x"
 docker compose exec php composer update --no-interaction
 
@@ -54,3 +52,16 @@ docker compose exec php vendor/bin/oe-console oe:setup:shop --db-host=mysql --db
   --compile-directory=/var/www/source/tmp/
 
 docker compose exec -T php vendor/bin/oe-console oe:database:reset --db-host=mysql --db-port=3306 --db-name=example --db-user=root --db-password=root --force
+
+docker compose exec -T php vendor/bin/oe-console oe:theme:activate apex
+
+docker compose exec -T php vendor/bin/oe-console oe:admin:create --admin-email="noreply@oxid-esales.com" --admin-password="admin"
+echo -e "\033[1;37m\033[1;42mCreate admin: Admin login: noreply@oxid-esales.com Password: admin\033[0m\n"
+
+# Register all related project packages git repositories
+cp ${SCRIPT_PATH}/../parts/bases/vcs.xml.base .idea/vcs.xml
+perl -pi\
+  -e 's#</component>#<mapping directory="\$PROJECT_DIR\$/source/vendor/oxid-esales/oxideshop-ce" vcs="Git" />\n  </component>#g;'\
+  -e 's#</component>#<mapping directory="\$PROJECT_DIR\$/source/vendor/oxid-esales/oxideshop-pe" vcs="Git" />\n  </component>#g;'\
+  -e 's#</component>#<mapping directory="\$PROJECT_DIR\$/source/vendor/oxid-esales/oxideshop-ee" vcs="Git" />\n  </component>#g;'\
+  .idea/vcs.xml
