be2350634830b75c87d623624fbf616472b643de
RahatHameed <the_rahat_online@yahoo.com>
2024-09-06 15:56:30 +0200

    OXDEV-8624 Create compile directory for shop v8.0

--- parts/shared/setup_database.sh
+++ parts/shared/setup_database.sh
@@ -21,9 +21,18 @@ flags "$@"
 
 echo -e "\033[1;37m\033[1;42mSetup shop\033[0m\n"
 
+# Set DIRECTORY_PATH based on the existence of /var/www/source/tmp/
+if [ -d "/var/www/source/tmp/" ]; then
+    DIRECTORY_PATH="/var/www/source/tmp"
+else
+    DIRECTORY_PATH="/var/www/var/cache"
+fi
+
+docker compose exec -T php sh -c "if [ ! -d '${DIRECTORY_PATH}' ]; then mkdir -p '${DIRECTORY_PATH}'; fi"
+
 docker compose exec php ${CONSOLE_PATH} oe:setup:shop --db-host=mysql --db-port=3306 --db-name=example --db-user=root \
   --db-password=root --shop-url=http://localhost.local/ --shop-directory=/var/www/source/ \
-  --compile-directory=/var/www/source/tmp/
+  --compile-directory="${DIRECTORY_PATH}/"
 
 $SHARED_SCRIPT_PATH/reset_database.sh
 
