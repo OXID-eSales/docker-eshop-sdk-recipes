5abe7a68873d47268765406cef5c6925604f25d2
Anton Fedurtsya <anton@fedurtsya.com>
2024-10-16 13:11:29 +0300

    OXDEV-8759 Remove parts directory
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- parts/shared/create_admin.sh
+++
@@ -1,7 +0,0 @@
-email=noreply@oxid-esales.com
-password=admin
-CONSOLE_PATH=$( [ -e "source/bin/oe-console" ] && echo "bin/oe-console" || echo "vendor/bin/oe-console" )
-
-docker compose exec -T php ${CONSOLE_PATH} oe:admin:create --admin-email="$email" --admin-password="$password"
-
-echo -e "\033[1;37m\033[1;42mCreate admin: Admin login: ${email} Password: ${password}\033[0m\n"
