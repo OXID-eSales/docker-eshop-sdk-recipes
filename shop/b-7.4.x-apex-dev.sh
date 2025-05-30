8824f132839397b02ef510390740a457b00037ab
Anton Fedurtsya <anton@fedurtsya.com>
2025-05-30 15:47:45 +0300

    Add shop 7.4 dev recipe
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ shop/b-7.4.x-apex-dev.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Flags possible:
+# -e for shop edition. Possible values: CE/PE/EE
+
+branch='b-7.4.x'
+edition='CE'
+while getopts e: flag; do
+  case "${flag}" in
+  e) edition=${OPTARG} ;;
+  *) ;;
+  esac
+done
+
+SCRIPT_PATH=$(dirname ${BASH_SOURCE[0]})
+cd $SCRIPT_PATH/../../../ || exit
+
+# Prepare services configuration
+make setup
+make addbasicservices
+make file=services/adminer.yml addservice
+make file=services/selenium-chrome.yml addservice
+make file=services/node.yml addservice
+
+# Configure containers
+perl -pi\
+  -e 's#error_reporting = .*#error_reporting = E_ALL ^ E_WARNING ^ E_DEPRECATED#g;'\
+  containers/php/custom.ini
+
+perl -pi\
+  -e 's#PHP_VERSION=.*#PHP_VERSION=8.2#g;'\
+  .env
+
+"${SCRIPT_PATH}/../parts/shared/prepare_shop_package.sh" -e"${edition}" -b"${branch}"
+"${SCRIPT_PATH}/../parts/shared/require_twig_components.sh" -e"${edition}" -b"${branch}"
+
+"${SCRIPT_PATH}/../parts/shared/require_theme_dev.sh" -t"apex" -b"${branch}"
+
+"${SCRIPT_PATH}/../parts/shared/require_demodata_package.sh" -e"${edition}" -b"${branch}"
+
+# Install all preconfigured dependencies
+docker compose exec -T php composer update --no-interaction
+
+# Setup the database
+"${SCRIPT_PATH}/../parts/shared/setup_database.sh"
+
+docker compose exec -T php bin/oe-console oe:theme:activate apex
+"${SCRIPT_PATH}/../parts/shared/create_admin.sh"
+
+echo "Done!"
+
+# after tests were executed sometimes reseting the db or cache is needed
+# afterwards
+# unit tests: vendor/bin/phpunit --testsuite Unit
+# integration tests: vendor/bin/phpunit --testsuite Integration
+# codeception tests: SELENIUM_SERVER_HOST=selenium BROWSER_NAME=chrome THEME_ID=apex vendor/bin/codecept run -c tests/codeception.yml -g xyz
+# can be executed
